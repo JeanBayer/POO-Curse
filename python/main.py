fa9349307f1ffc4c1812f8a4a8c53cfca2fda742
JeanBayer <jhanbayer@gmail.com>
2020-06-23 16:32:07 -0400

    haciendo la instancia en python con la clase car

--- python/main.py
+++ python/main.py
@@ -1,2 +1,14 @@
+from car import Car
 if __name__ ==  "__main__":
-    print("Hola mundo")    
+    print("Hola mundo")
+
+    car = Car()
+    car.license = "AMS234"
+    car.driver = "Andres Herrera"
+    print(vars(car))  #vars y dentro la variable, imprime todo lo que esta contenga
+
+    car2 = Car()
+    car2.license = "QWE567" 
+    car2.driver = "Andrea Herrera"
+    print(vars(car2))
+     
