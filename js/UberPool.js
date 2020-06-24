646c10b429209706694c12dc6b448f31d18dd52d
JeanBayer <jhanbayer@gmail.com>
2020-06-24 17:31:50 -0400

    Herencia en Javascript

---
+++ js/UberPool.js
@@ -0,0 +1,10 @@
+class UberPool extends Car {
+
+    constructor(license, driver, brand, model){
+        super(license, driver)
+        this.brand = brand;
+        this.model = model;
+
+    }
+
+}
--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
 var car = new Car("AW456", new Account("Andres Herrera", "AND123"))
 car.passenger = 4;
 car.printDataCar();
+
+var uberX = new UberX("AW456", new Account("Andres Herrera UberX", "AND123"), "Chevrolet", "Spark")
+uberX.passenger = 4;
+uberX.printDataCar();
