420be95ba5bb6d7b2c5ce8766e76369f777d4d38
JeanBayer <jhanbayer@gmail.com>
2020-06-23 22:27:18 -0400

    añadí metodo constructor en python

--- python/main.py
+++ python/main.py
@@ -1,14 +1,13 @@
 from car import Car
+from account import Account
+
 if __name__ ==  "__main__":
     print("Hola mundo")
 
-    car = Car()
-    car.license = "AMS234"
-    car.driver = "Andres Herrera"
+    car = Car("AMS234", Account("Andres Herrera", "ANDA876"))
     print(vars(car))  #vars y dentro la variable, imprime todo lo que esta contenga
+    print(vars(car.driver))
 
-    car2 = Car()
-    car2.license = "QWE567" 
-    car2.driver = "Andrea Herrera"
+    car2 = Car("QWE567", Account("Andrea Herrera", "AND123"))
     print(vars(car2))
      
